faa97ade2a90fb88b835f449efecf2f5bf681d2a
Marcus Quigley <marcusquigley@gmail.com>
2020-10-12 17:34:31 -0400

    Arrays Easy Fibonacci

--- src/main/java/com/leetcode/tests/arrays/ArraysProblemsEasy.java
+++ src/main/java/com/leetcode/tests/arrays/ArraysProblemsEasy.java
@@ -1,5 +1,47 @@
 package com.leetcode.tests.arrays;
 
 public class ArraysProblemsEasy {
+    //https://leetcode.com/problems/fibonacci-number/
+    public static int fibTopDown(int N) {
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+        return fibTopDown(N - 1) + fibTopDown(N - 2);
+    }
 
+    public static int fibMemoTopDown(int N) {
+        return fibMemoTopDownWorker(N, new int[N+1]);
+    }
+
+    static int fibMemoTopDownWorker(int n, int[] memo) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (memo[n] == 0)
+            memo[n] = fibMemoTopDownWorker(n-1, memo) + fibMemoTopDownWorker(n-2, memo);
+        return memo[n];
+    }
+
+    public static int fibMemoBottomUp(int n) {
+        int[] memo = new int[n+1];
+        memo[0] = 0;
+        memo[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            memo[i] = memo[i-1] + memo[i-2];
+        }
+        return  memo[n];
+    }
+
+    public static int fibBottomUpNoMemo(int n) {
+        if (n==0) return 0;
+        int twoBack = 0;
+        int oneBack = 1;
+        for (int i = 2; i < n; i++) {
+            int temp = twoBack + oneBack;
+            twoBack = oneBack;
+            oneBack = temp ;
+        }
+        return  twoBack + oneBack;
+    }
 }
+
+
+
--- src/test/java/com/leetcode/tests/arrays/ArraysProblemsEasyTest.java
+++ src/test/java/com/leetcode/tests/arrays/ArraysProblemsEasyTest.java
@@ -1,4 +1,53 @@
 package com.leetcode.tests.arrays;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 public class ArraysProblemsEasyTest {
+    static Stream<Arguments> source_fib() {
+        return Stream.of(arguments(3, 2)
+                , arguments(1, 1)
+                , arguments(2, 1)
+                , arguments(3, 2)
+                , arguments(4, 3)
+                , arguments(5,5)
+                , arguments(6, 8)
+                , arguments(8, 21)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("source_fib")
+    void test_fibTopDown(int n, int expected) {
+        int actual = ArraysProblemsEasy.fibTopDown(n);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("source_fib")
+    void test_fibMemoTopDown(int n, int expected) {
+        int actual = ArraysProblemsEasy.fibMemoTopDown(n);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("source_fib")
+    void test_fibMemoBottomUp(int n, int expected) {
+        int actual = ArraysProblemsEasy.fibMemoBottomUp(n);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("source_fib")
+    void test_fibBottomUpNoMemo(int n, int expected) {
+        int actual = ArraysProblemsEasy.fibBottomUpNoMemo(n);
+        assertEquals(expected, actual);
+    }
+
 }
