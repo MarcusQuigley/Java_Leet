573bdc9b6b848161a1f069253aabf65ddc215431
Marcus Quigley <marcusquigley@gmail.com>
2020-10-13 10:29:00 -0400

    Recursion Exercises Reverse String

--- src/main/java/com/leetcode/recursion/RecursionLearning.java
+++ src/main/java/com/leetcode/recursion/RecursionLearning.java
@@ -2,15 +2,38 @@ package com.leetcode.recursion;
 
 public class RecursionLearning {
 
-    public static void printReverse(char [] str) {
-        printReverseWorker(str, str.length-1);
+    public static void printReverse(char[] str) {
+        printReverseWorker(str, str.length - 1);
     }
 
     private static void printReverseWorker(char[] str, int i) {
         if (i < 0)
             return;
         System.out.print(str[i]);
-        printReverseWorker(str, i-1);
+        printReverseWorker(str, i - 1);
     }
 
+    public static void reverseString(char[] s) {
+        System.out.println("Begin");
+        printArray(s);
+        reverseStringWorker(s, 0, s.length - 1);
+        System.out.println("End");
+        printArray(s);
+    }
+
+    static void reverseStringWorker(char[] s, int begin, int end) {
+        if (begin < end) {
+            char temp = s[begin];
+            s[begin] = s[end];
+            s[end] = temp;
+            //begin++; end--;
+            reverseStringWorker(s, begin+1, end-1);
+        }
+    }
+
+    static void printArray(char[] s) {
+        for (int i = 0; i < s.length; i++) {
+            System.out.print(s[i]);
+        }
+    }
 }
--- src/main/java/com/leetcode/runner/RunProblems.java
+++ src/main/java/com/leetcode/runner/RunProblems.java
@@ -4,6 +4,7 @@ import com.leetcode.recursion.RecursionLearning;
 
 public class RunProblems{
     public static void main(String[] args) {
-        RecursionLearning.printReverse("Hello".toCharArray());
+        //RecursionLearning.printReverse("Hello".toCharArray());
+        RecursionLearning.reverseString("ReverseString".toCharArray());
     }
 }
