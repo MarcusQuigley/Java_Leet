038ec815db77b16b923c6b7bd9db6bbe8d66381b
marcus quigley <marcusquigley@gmail.com>
2020-10-23 11:42:56 -0400

    Trees Easy Trim BST

--- src/main/java/com/leetcode/trees/TreesProblemsEasy.java
+++ src/main/java/com/leetcode/trees/TreesProblemsEasy.java
@@ -452,4 +452,28 @@ public class TreesProblemsEasy {
 		}
 		return list;
 	}
+
+	public static TreeNode trimBST(TreeNode root, int low, int high) {
+		if (root == null)
+			return null;
+		if (root.val == low) {
+			root.left = null;
+			root.right = trimBST(root.right, low, high);
+			return root;
+		}
+		if (root.val == high) {
+			root.right = null;
+			root.left = trimBST(root.left, low, high);
+			return root;
+		}
+		if (root.val > high) {
+			return trimBST(root.left, low, high);
+		}
+		if (root.val < low) {
+			return trimBST(root.right, low, high);
+		}
+		root.left = trimBST(root.left, low, high);
+		root.right = trimBST(root.right, low, high);
+		return root;
+	}
 }
